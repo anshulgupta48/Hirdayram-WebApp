58a03691b984dfb89d071cb25ba6534310591a53
anshulgupta48 <anshulgupta4905@gmail.com>
2025-07-15 13:09:54 +0530

    Adding Home-Page Responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,35 +58,35 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
 
   return (
     <div className='flex flex-col font-sans'>
-      <div className='min-h-[600px] w-full pt-[200px] pb-[100p] px-[200px] hero-bg flex flex-col gap-[30px] relative'>
-        <img src={HeroOverlay} alt="Hero-Overlay" className='absolute right-0 bottom-0 w-[50%]' />
-        <h2 className='w-[470px] text-[#16171A] text-[65px] font-bold leading-[65px]'>Electricity is really organized just ligh<span className='text-[#DD0733]'>ting</span></h2>
-        <p className='w-[520px] text-[#727272] text-[14px]'>Expert & Experienced Electrician for Residential to Commercial to Service with 100% satisfaction guarantee</p>
+      <div className='min-h-[400px] lg:min-h-[600px] w-full pt-[100px] sm:pt-[150px] lg:pt-[200px] pb-[30px] sm:pb-[50px] lg:pb-[100p] px-[30px] sm:px-[50px] lg:px-[200px] hero-bg flex flex-col gap-[30px] relative'>
+        <img src={HeroOverlay} alt="Hero-Overlay" className='hidden lg:flex absolute right-0 bottom-0 w-[50%]' />
+        <h2 className='sm:w-[470px] text-[#16171A] text-[35px] sm:text-[50px] md:text-[65px] font-bold leading-[40px] sm:leading-[58px] md:leading-[65px]'>Electricity is really organized just ligh<span className='text-[#DD0733]'>ting</span></h2>
+        <p className='sm:w-[520px] text-[#727272] text-[14px]'>Expert & Experienced Electrician for Residential to Commercial to Service with 100% satisfaction guarantee</p>
 
         <button className='h-[40px] w-[155px] bg-[#DD0733] btn-hover flex justify-center items-center relative'>
           <span className='text-[#FFFFFF] text-[12px] uppercase font-medium z-10'>Discover More</span>
         </button>
       </div>
 
-      <div className='min-h-[600px] mx-[10px] about-bg flex items-center gap-[40px] overflow-hidden' style={{ width: 'calc(100% - 20px)' }}>
-        <div className='h-full w-[50%] flex justify-end items-center'>
-          <div className='max-w-[385px] flex flex-col gap-[10px] relative right-[50px]'>
+      <div className={`min-h-[420px] sm:min-h-[450px] lg:min-h-[600px] mx-[10px] py-[30px] sm:py-0 flex items-center overflow-hidden ${window.innerWidth > 640 ? 'about-bg' : 'bg-[#FFFFFF]'}`} style={{ width: 'calc(100% - 20px)' }}>
+        <div className='h-full w-full lg:w-[50%] px-[20px] sm:pl-[25%] lg:pl-0 flex justify-center sm:justify-start lg:justify-end items-center'>
+          <div className='w-full lg:max-w-[385px] flex flex-col gap-[10px] relative sm:right-[50px]'>
             <div className='flex items-center gap-[3px]'>
               <IoIosFlash className='text-[#DD0733] text-[16px]' />
               <p className='text-[#727272] text-[12px] font-medium uppercase'>We are Hirdayram Engineering Organization</p>
             </div>
 
-            <h3 className='text-[#16171A] text-[35px] font-bold leading-[38px]'>One Stop Electrical & Renewable Energy Solutions</h3>
+            <h3 className='text-[#16171A] text-[30px] sm:text-[35px] font-bold leading-[32px] sm:leading-[38px]'>One Stop Electrical & Renewable Energy Solutions</h3>
             <p className='mt-[5px] text-[#727272] text-[12px]'>We provide electrical maintenance, motor rewinding, electrical EPC, lighting & CCTV camera, cabling and transmission line erection and commissioning services. </p>
             <p className='mt-[5px] text-[#DD0733] text-[14px]'>We create experience and build products that make business grow</p>
 
-            <div className='mt-[10px] pb-[15px] border-b border-solid border-[#F0F0F0] flex items-center gap-[30px]'>
+            <div className='mt-[10px] pb-[15px] border-b border-solid border-[#F0F0F0] flex flex-col sm:flex-row sm:items-center gap-[30px]'>
               <div className='flex items-center gap-[15px]'>
                 <img src={ExpertElectricianIcon} alt="Expert-Electrician-Icon" className='h-[60px] w-[60px]' />
                 <p className='w-[80px] text-[#16171A] text-[15px] font-bold'>Expert electrician</p>
               </div>
 
-              <img src={TriangleShapeIcon} alt="Triangle-Icon" className='h-[15px] w-[20px]' />
+              <img src={TriangleShapeIcon} alt="Triangle-Icon" className='hidden sm:flex h-[15px] w-[20px]' />
 
               <div className='flex items-center gap-[15px]'>
                 <img src={SafetyAssuranceIcon} alt="Safety-Assurance-Icon" className='h-[60px] w-[60px]' />
@@ -102,27 +102,27 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
           </div>
         </div>
 
-        <div className='h-full w-[50%] flex justify-center relative'>
+        <div className='h-full w-[50%] hidden lg:flex justify-center relative'>
           <img src={AboutBanner1} alt="About-Banner3" className='absolute left-[-20px] top-[50px] h-[120px] w-[120px] animate-spin z-10' style={{ animationDuration: '10s' }} />
           <img src={AboutBanner2} alt="About-Banner2" className='h-[358px] w-[334px]' />
           <img src={AboutBanner3} alt="About-Banner3" className='absolute left-0 top-[220px] h-[188px] w-[284px] z-10' />
         </div>
       </div>
 
-      <div className='min-h-[600px] w-full services-bg flex flex-col justify-center items-center gap-[40px] relative services-container'>
-        <img src={ServicesOverlay} alt="Services-Overlay" className='absolute right-0 top-[40px] h-[125px] w-[125px] topToDownAnimation' />
+      <div className='min-h-[550px] lg:min-h-[600px] w-full services-bg flex flex-col justify-center items-center gap-[40px] relative services-container'>
+        <img src={ServicesOverlay} alt="Services-Overlay" className='hidden lg:flex absolute right-0 top-[40px] h-[125px] w-[125px] topToDownAnimation' />
 
-        <div className='flex flex-col gap-[5px]'>
+        <div className='px-[10px] sm:px-0 flex flex-col gap-[5px]'>
           <div className='flex justify-center items-center gap-[3px]'>
             <IoIosFlash className='text-[#DD0733] text-[16px]' />
             <p className='text-[#727272] text-[12px] font-medium uppercase'>Hirdayram Services</p>
           </div>
 
-          <h3 className='w-[560px] text-[#16171A] text-[36px] font-bold text-center leading-[40px]'>Outstanding power plant, engineering & residential services</h3>
+          <h3 className='w-full sm:w-[560px] text-[#16171A] text-[30px] sm:text-[36px] font-bold text-center leading-[34px] sm:leading-[40px]'>Outstanding power plant, engineering & residential services</h3>
         </div>
 
-        <div className='w-full flex justify-between items-center gap-[30px]'>
-          <Swiper onSwiper={(swiper) => (servicesCarouselRef.current = swiper)} slidesPerView={3} spaceBetween={30} speed={1200} loop={true} modules={[Pagination, Navigation]} className='mySwiper'>
+        <div className='w-[80%] flex justify-between items-center gap-[30px] relative left-[20px] sm:left-0'>
+          <Swiper onSwiper={(swiper) => (servicesCarouselRef.current = swiper)} slidesPerView={window.innerWidth > 1024 ? 3 : window.innerWidth > 640 ? 2 : 1} spaceBetween={30} speed={1200} loop={true} modules={[Pagination, Navigation]} className='mySwiper' style={{ width: window.innerWidth > 1024 ? '840px' : window.innerWidth > 640 ? '550px' : '300px' }}>
             {servicesData.map((item, index) => (
               <SwiperSlide className='w-[260px]' key={index}>
                 <div className='h-[310px] w-[260px] bg-[#FFFFFF] flex flex-col group'>
@@ -144,19 +144,19 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
         </div>
       </div>
 
-      <div className='min-h-[600px] w-full choose-us-bg flex gap-[30px] relative'>
-        <div className='h-full w-[50%] pt-[80px] flex justify-end'>
+      <div className='min-h-[700px] sm:min-h-[550px] lg:min-h-[600px] px-[30px] lg:px-0 w-full choose-us-bg flex gap-[30px] relative'>
+        <div className='h-full w-full lg:w-[50%] pt-[30px] lg:pt-[80px] flex justify-start lg:justify-end'>
           <div className='max-w-[450px] flex flex-col gap-[30px]'>
-            <img src={ChooseUsOverlay1} alt="ChooseUs-Overlay-1" className='absolute left-[50px] bottom-[50px] h-[90px] w-[170px] leftToRightAnimation z-10' />
+            <img src={ChooseUsOverlay1} alt="ChooseUs-Overlay-1" className='absolute left-[50px] bottom-[30px] sm:bottom-[50px] h-[90px] w-[170px] leftToRightAnimation z-10' />
 
             <div className='flex flex-col gap-[3px]'>
               <p className='text-[#9F9F9F] text-[12px] uppercase font-medium'>Why Choose our Hirdayram</p>
-              <h3 className='text-[#FFFFFF] text-[35px] font-semibold leading-[35px]'>Great reason for the people choose Hirdayram</h3>
+              <h3 className='text-[#FFFFFF] text-[30px] sm:text-[35px] font-semibold leading-[32px] sm:leading-[35px]'>Great reason for the people choose Hirdayram</h3>
             </div>
 
             <p className='text-[#FFFFFFA3] text-[12px]'>Professional, reliable, accountable, experienced, on time work completion, in house engineering and designing, fully customer centric.</p>
 
-            <div className='w-[450px] flex items-center gap-[15px] flex-wrap'>
+            <div className='sm:w-[450px] flex flex-col sm:flex-row sm:items-center gap-[15px] flex-wrap'>
               <div className='h-[60px] w-[200px] px-[15px] bg-[#212226] flex items-center gap-[10px]'>
                 <img src={CustomSupportIcon} alt="CustomSupport-Icon" className='h-[45px] w-[45px] rounded-full' />
                 <p className='text-[#FFFFFF] text-[14px] font-bold leading-[17px]'>Custom support</p>
@@ -180,27 +180,27 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
           </div>
         </div>
 
-        <div className='w-[50%] flex justify-end overflow-hidden relative'>
+        <div className='w-[50%] hidden lg:flex justify-end overflow-hidden relative'>
           <img src={ChooseUsBanner1} alt="ChooseUs-Banner-1" className='h-[417px] w-[497px]' />
           <img src={ChooseUsOverlay2} alt="ChooseUs-Overlay-2" className='absolute right-0 bottom-0 h-[337px] w-[617px] leftToRightAnimation z-10' />
           <div className='absolute right-[-150px] bottom-[-60px] h-[500px] w-[350px] bg-[#16171A] rotate-[47deg] z-20'></div>
         </div>
       </div>
 
-      <div className='min-h-[460px] w-full px-[70px] clients-review-bg flex flex-col justify-center items-center gap-[40px] clients-review-container'>
+      <div className='min-h-[400px] sm:min-h-[460px] w-full px-[20px] sm:px-[50px] lg:px-[70px] py-[30px] sm:py-0 clients-review-bg hidden lg:flex flex-col justify-center items-center gap-[40px] clients-review-container'>
         <div className='flex flex-col gap-[5px]'>
           <div className='flex justify-center items-center gap-[3px]'>
             <IoIosFlash className='text-[#DD0733] text-[16px]' />
             <p className='text-[#727272] text-[12px] font-medium uppercase'>Our Clients Review</p>
           </div>
 
-          <h3 className='w-[450px] text-[#16171A] text-[36px] font-bold text-center leading-[40px]'>What they're talking about Hirdayram Services</h3>
+          <h3 className='w-full sm:w-[450px] text-[#16171A] text-[30px] sm:text-[36px] font-bold text-center leading-[32px] sm:leading-[40px]'>What they're talking about Hirdayram Services</h3>
         </div>
 
         <div className='w-full flex items-center gap-[80px]'>
-          <Swiper onSwiper={(swiper) => (clientReviewsCarouselRef.current = swiper)} slidesPerView={2} spaceBetween={80} speed={1200} loop={true} modules={[Pagination, Navigation]} className='mySwiper'>
+          <Swiper onSwiper={(swiper) => (clientReviewsCarouselRef.current = swiper)} slidesPerView={window.innerWidth > 1024 ? 2 : 1} spaceBetween={80} speed={1200} loop={true} modules={[Pagination, Navigation]} className='mySwiper'>
             {clientReviewsData.map((item, index) => (
-              <SwiperSlide className='max-w-[350px]' key={index}>
+              <SwiperSlide className='max-w-[300px] sm:max-w-[350px]' key={index}>
                 <div className='h-[180px] w-[350px] pl-[60px] pr-[10px] clients-review-box-bg flex flex-col justify-center gap-[20px] relative'>
                   <p className='w-[270px] text-[#727272] text-[12px] leading-[18px]'>{item.description}</p>
 
@@ -232,17 +232,17 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
         </div>
       </div>
 
-      <div className='min-h-[600px] w-full pt-[70px] bg-[#FFFFFF] flex flex-col items-center gap-[40px] relative'>
+      <div className='min-h-[500px] lg:min-h-[600px] w-full px-[30px] py-[30px] md:py-[50px] lg:pt-[70px] bg-[#FFFFFF] flex flex-col items-center gap-[40px] relative'>
         <div className='flex flex-col gap-[5px]'>
           <div className='flex justify-center items-center gap-[3px]'>
             <IoIosFlash className='text-[#DD0733] text-[16px]' />
             <p className='text-[#727272] text-[12px] font-medium uppercase'>Our Article and Blog</p>
           </div>
 
-          <h3 className='w-[400px] text-[#16171A] text-[32px] font-bold text-center leading-[40px]'>Latest news & articles from the blog</h3>
+          <h3 className='w-full sm:w-[400px] text-[#16171A] text-[28px] sm:text-[32px] font-bold text-center leading-[32px] sm:leading-[40px]'>Latest news & articles from the blog</h3>
         </div>
 
-        <div className='w-[840px] flex justify-between items-center gap-[30px]'>
+        <div className='w-full lg:w-[840px] flex flex-col lg:flex-row justify-between items-center flex-wrap gap-[30px]'>
           {blogsData.map((item, index) => (
             <div className='h-[280px] w-[260px] bg-[#FFFFFF] flex flex-col blogs-box-shadow group' key={index}>
               <img src={item.banner} alt="Blog-Banner" className='h-[170px] w-[260px]' />
@@ -256,7 +256,7 @@ const Home: React.FunctionComponent<HomePageProps> = ({ setSelectedServiceDetail
           ))}
         </div>
 
-        <div className='absolute bottom-[-50px] left-[50%] translate-x-[-50%] h-[100px] w-[800px] px-[40px] bg-[#DD0733] flex justify-between items-center gap-[60px] z-20'>
+        <div className='absolute bottom-[-50px] left-[50%] translate-x-[-50%] h-[100px] w-[800px] px-[40px] bg-[#DD0733] hidden lg:flex justify-between items-center gap-[60px] z-20'>
           <h3 className='w-[400px] text-[#FFFFFF] text-[22px] font-bold leading-[28px]'>Dedicated to bring the world powerful energy solutions</h3>
 
           <div className='h-[60px] w-[400px] px-[10px] border-1 border-solid border-[#E38B9D] flex items-center gap-[5px]'>
